a64a1289ebd32875f17cb41becd5db698386d15d
Geunyoung Kim <emsud@inu.ac.kr>
2019-06-04 15:40:26 +0900

    adsf

---
+++ setting.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 14:48:08 2019
+
+@author: emsud
+"""
+import os
+
+path=os.getcwd()
+Data_PATH="../data/"
+Dir_name="data"
+Save_PATH="../data/"
+Driver_PATH="/Users/emsud/chromedriver_win32/chromedriver"
+yes24_PATH="http://www.yes24.com"
+Product_PATH="/Product/Goods/"
